8a0d4a8d3852eb5b6cb2f2ff3897263b2a363c99
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2022-02-04 16:55:28 -0300

    Aula 40

--- src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
+++ src/app/components/views/categoria/categoria-delete/categoria-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CategoriaService } from '../categoria.service';
+import { Categoria } from '../categoria.model';
 
 @Component({
   selector: 'app-categoria-delete',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaDeleteComponent implements OnInit {
 
-  constructor() { }
+  categoria: Categoria = {
+    id: '',
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(private service: CategoriaService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!
+    this.findById()
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe((resposta) => {
+      this.categoria.nome = resposta.nome
+      this.categoria.descricao = resposta.descricao    
+    }
+
+    )
+  }
+
+  cancel(): void{
+    this.router.navigate(['categorias']);
+  }
+
+  deletar(): void{
+
   }
 
 }
