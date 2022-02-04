1337f572847173646eda1b6c0e0e9ef9dd855c7b
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2022-02-04 15:52:57 -0300

    Aula 37

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Categoria } from './categoria.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class CategoriaService {
 
   baseUrl: String = environment.baseUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
     findAll():Observable<Categoria[]>{
       const url = `${this.baseUrl}/categorias`
@@ -21,6 +22,15 @@ export class CategoriaService {
     create(categoria: Categoria): Observable<Categoria>{
       const url = `${this.baseUrl}/categorias`;
       return this.http.post<Categoria>(url, categoria);
+
+    }
+
+    mensagem(str: String): void{
+      this._snack.open(`${str}`, 'OK', {
+        horizontalPosition: 'end',
+        verticalPosition: 'top',
+        duration: 3000
+      })
 
     }
 }
